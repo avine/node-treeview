353b3a6377ce8301ff83d4ac42d87947e2db6a9b
Stéphane Francel <contact@avine.io>
2017-11-07 17:00:48 +0100

    Improve `require.spec.ts`

--- spec/require.spec.ts
+++ spec/require.spec.ts
@@ -2,24 +2,19 @@
 
 const resolve = require('path').resolve;
 
-// After the command `npm run build` is executed, this script location is:
-//      `[node-treeview]/dist/spec/require.spec.js`.
-//
-// From this location, the `[node-treeview]/dist/src` folder location is:
+// The folder published to NPM is: `[node-treeview]/dist/src`
 const npmPkgRoot = resolve(__dirname, '../src');
 
-// From `npmPkgRoot` (which is the root of the folder published to NPM),
-// the packaged scripts locations are:
-const indexJS = './index.js';
-const flattenJS = './helper/flatten.js';
-// const cliJS = './bin/cli.js';
-
-const TreeView = require(resolve(npmPkgRoot, indexJS)).TreeView;
-const flatten = require(resolve(npmPkgRoot, flattenJS)).flatten;
+const path2Index = resolve(npmPkgRoot/*, './index'*/);
+const path2Flatten = resolve(npmPkgRoot, './helper/flatten');
 
 describe('TreeView require', () => {
-  // Simply check that `TreeView` and `flatten` can be used...
-  it('should find TreeView() and flatten()', (done) => {
+  it('should require and use TreeView() and flatten()', (done) => {
+    // Check that `TreeView` and `flatten` can be required...
+    const TreeView = require(path2Index).TreeView;
+    const flatten = require(path2Flatten).flatten;
+
+    // ...and used ;-)
     new TreeView().process(__dirname).then((result: any) => {
       const flat = flatten(result);
       // console.log(JSON.stringify(flat, undefined, 2));
