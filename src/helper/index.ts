9add2254b14a1cdbe68597947659dc2fd36d6a86
Stéphane Francel <contact@avine.io>
2018-05-06 01:58:18 +0200

    Clean useless files and packages

--- src/helper/index.ts
+++ src/helper/index.ts
@@ -1,6 +1,5 @@
 import { clean } from './clean';
 import { flatten } from './flatten';
 import { pretty } from './pretty';
-import { watch } from './watch';
 
-export { clean, flatten, pretty, watch };
+export { clean, flatten, pretty };
